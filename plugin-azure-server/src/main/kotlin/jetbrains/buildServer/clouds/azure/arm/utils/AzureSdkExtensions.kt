a21305b6a9b0252b449fcb8d37ebcdda77e6a24c
Dmitry Tretyakov <dtretyakov@gmail.com>
2018-10-16 18:37:57 +0200

    Correct awaiters for rxjava
    Before that change was not handled onCompleted call for Observables and
    was possibility try to resume non active continuation.
    
    Issue: TW-56971

--- plugin-azure-server/src/main/kotlin/jetbrains/buildServer/clouds/azure/arm/utils/AzureSdkExtensions.kt
+++ plugin-azure-server/src/main/kotlin/jetbrains/buildServer/clouds/azure/arm/utils/AzureSdkExtensions.kt
@@ -17,16 +17,24 @@
 package jetbrains.buildServer.clouds.azure.arm.utils
 
 import kotlinx.coroutines.experimental.suspendCancellableCoroutine
-import rx.Completable
-import rx.Observable
+import rx.*
+import java.lang.IllegalStateException
 
 suspend fun Completable.awaitOne(): Unit = suspendCancellableCoroutine { cont ->
-    val subscription = subscribe({
-        if (cont.isActive) {
-            cont.resume(Unit)
+    lateinit var subscription: Subscription
+
+    subscribe(object : CompletableSubscriber {
+        override fun onSubscribe(d: Subscription) {
+            subscription = d
         }
-    }, { e ->
-        cont.resumeWithException(e!!)
+
+        override fun onError(e: Throwable) {
+            if (cont.isActive) cont.resumeWithException(e)
+        }
+
+        override fun onCompleted() {
+            if (cont.isActive) cont.resume(Unit)
+        }
     })
 
     cont.invokeOnCancellation {
@@ -34,13 +42,55 @@ suspend fun Completable.awaitOne(): Unit = suspendCancellableCoroutine { cont ->
     }
 }
 
+@Suppress("UNCHECKED_CAST")
 suspend fun <T> Observable<T>.awaitOne(): T = suspendCancellableCoroutine { cont ->
-    val subscription = subscribe({ r ->
-        if (cont.isActive) {
-            cont.resume(r)
+    lateinit var subscription: Subscription
+    var value: T? = null
+    var seenValue = false
+    var completed = false
+
+    subscription = subscribe(object : Subscriber<T>() {
+        override fun onNext(t: T) {
+            if (completed) {
+                if (cont.isActive) {
+                    cont.resumeWithException(IllegalStateException("Received onNext call after completion"))
+                }
+                return
+            }
+
+            if (seenValue) {
+                if (cont.isActive) {
+                    cont.resumeWithException(IllegalArgumentException("More than one onNext call"))
+                }
+                subscription.unsubscribe()
+            } else {
+                value = t
+                seenValue = true
+            }
         }
-    }, { e ->
-        cont.resumeWithException(e)
+
+        override fun onCompleted() {
+            if (completed) {
+                if (cont.isActive) {
+                    cont.resumeWithException(IllegalStateException("Received onCompleted call after completion"))
+                }
+                return
+            }
+
+            completed = true
+
+            if (!cont.isActive) return
+
+            if (seenValue) {
+                cont.resume(value as T)
+            } else {
+                cont.resumeWithException(NoSuchElementException("No value received via onNext call"))
+            }
+        }
+
+        override fun onError(e: Throwable) {
+            if (cont.isActive) cont.resumeWithException(e)
+        }
     })
 
     cont.invokeOnCancellation {
@@ -48,16 +98,4 @@ suspend fun <T> Observable<T>.awaitOne(): T = suspendCancellableCoroutine { cont
     }
 }
 
-suspend fun <T> Observable<T>.awaitList(): List<T> = suspendCancellableCoroutine { cont ->
-    val subscription = toList().subscribe({ r ->
-        if (cont.isActive) {
-            cont.resume(r)
-        }
-    }, { e ->
-        cont.resumeWithException(e)
-    })
-
-    cont.invokeOnCancellation {
-        subscription.unsubscribe()
-    }
-}
+suspend fun <T> Observable<T>.awaitList(): List<T> = toList().awaitOne()

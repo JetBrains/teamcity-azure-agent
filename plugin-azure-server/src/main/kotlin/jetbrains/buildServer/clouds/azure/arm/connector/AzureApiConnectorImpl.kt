46699b17dde63d906c182b8a62920126e4762f7b
Dmitry Tretyakov <dtretyakov@gmail.com>
2019-01-08 16:14:04 +0100

    Revert "Fix problem win container instance deployment into specific group"
    This reverts commit f7f13073

--- plugin-azure-server/src/main/kotlin/jetbrains/buildServer/clouds/azure/arm/connector/AzureApiConnectorImpl.kt
+++ plugin-azure-server/src/main/kotlin/jetbrains/buildServer/clouds/azure/arm/connector/AzureApiConnectorImpl.kt
@@ -561,7 +561,14 @@ class AzureApiConnectorImpl(params: Map<String, String>)
                 .setTags(VM_RESOURCE_NAME, instance.properties)
 
         val details = instance.image.imageDetails
-        val groupId = getResourceGroup(details, name)
+        val groupId = when (details.target) {
+            AzureCloudDeployTarget.NewGroup -> {
+                createResourceGroup(name, details.region!!)
+                name
+            }
+            AzureCloudDeployTarget.SpecificGroup -> details.groupId!!
+            else -> throw CloudException("Creating virtual machine $name is prohibited")
+        }
 
         builder.logDetails()
         val template = builder.toString()
@@ -577,7 +584,14 @@ class AzureApiConnectorImpl(params: Map<String, String>)
         val builder = handler!!.prepareBuilder(instance)
 
         val details = instance.image.imageDetails
-        val groupId = getResourceGroup(details, name)
+        val groupId = when (details.target) {
+            AzureCloudDeployTarget.NewGroup -> {
+                createResourceGroup(details.sourceId, details.region!!)
+                details.sourceId
+            }
+            AzureCloudDeployTarget.SpecificGroup -> details.groupId!!
+            else -> throw CloudException("Creating container $name is prohibited")
+        }
 
         if ("Linux" == details.osType && details.storageAccount != null) {
             addContainerCustomData(instance, userData, builder)

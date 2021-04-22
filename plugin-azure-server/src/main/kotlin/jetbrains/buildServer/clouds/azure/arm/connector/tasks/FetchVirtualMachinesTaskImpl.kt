ded837e21ad0d824e2b44c6deebe76b59944db12
Ilya Fomenko <ilya.fomenko@jetbrains.com>
2021-04-22 12:58:50 +0300

    Added logging and exception handling while reading VM info

--- plugin-azure-server/src/main/kotlin/jetbrains/buildServer/clouds/azure/arm/connector/tasks/FetchVirtualMachinesTaskImpl.kt
+++ plugin-azure-server/src/main/kotlin/jetbrains/buildServer/clouds/azure/arm/connector/tasks/FetchVirtualMachinesTaskImpl.kt
@@ -16,6 +16,7 @@
 
 package jetbrains.buildServer.clouds.azure.arm.connector.tasks
 
+import com.intellij.openapi.diagnostic.Logger
 import com.microsoft.azure.management.Azure
 import com.microsoft.azure.management.compute.OperatingSystemTypes
 import jetbrains.buildServer.clouds.azure.arm.throttler.AzureThrottlerCacheableTaskBaseImpl
@@ -34,22 +35,36 @@ class FetchVirtualMachinesTaskImpl : AzureThrottlerCacheableTaskBaseImpl<Unit, L
         return api
                 .virtualMachines()
                 .listAsync()
-                .map {
-                    FetchVirtualMachinesTaskVirtualMachineDescriptor(
-                            it.id(),
-                            it.name(),
-                            it.resourceGroupName(),
-                            it.isManagedDiskEnabled,
-                            it.osUnmanagedDiskVhdUri(),
-                            when(it.osType()) {
-                                OperatingSystemTypes.LINUX -> "Linux"
-                                OperatingSystemTypes.WINDOWS -> "Windows"
-                                else -> it.osType()?.name
-                            }
-                    )
-                }
                 .toList()
                 .last()
+                .map { list ->
+                    val result = mutableListOf<FetchVirtualMachinesTaskVirtualMachineDescriptor>()
+                    for(vm in list) {
+                        try {
+                            result.add(
+                                    FetchVirtualMachinesTaskVirtualMachineDescriptor(
+                                            vm.id(),
+                                            vm.name(),
+                                            vm.resourceGroupName(),
+                                            vm.isManagedDiskEnabled,
+                                            if (vm.isManagedDiskEnabled) null else vm.inner().storageProfile().osDisk()?.vhd()?.uri(),
+                                            when (vm.osType()) {
+                                                OperatingSystemTypes.LINUX -> "Linux"
+                                                OperatingSystemTypes.WINDOWS -> "Windows"
+                                                else -> vm.osType()?.name
+                                            }
+                                    )
+                            )
+                        } catch (exception: Throwable) {
+                            LOG.warnAndDebugDetails("Could not read VirtualMachine. Id=${vm.id()}, Name=${vm.name()}, ResourceGroup=${vm.resourceGroupName()}", exception)
+                        }
+                    }
+                    result.toList()
+                }
                 .toSingle()
+    }
+
+    companion object {
+        private val LOG = Logger.getInstance(FetchVirtualMachinesTaskImpl::class.java.name)
     }
 }
